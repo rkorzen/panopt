4d8d51457c2b02b1cb66a25d16d52198871aee63
rkorzen <korzeniewski@gmail.com>
2022-06-27 14:54:10 +0200

    fix CSRF_TRUSTED_ORIGIN

--- panopt/settings.py
+++ panopt/settings.py
@@ -122,7 +122,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ["https://fierce-thicket-99081.herokuapp.com/"]
+CSRF_TRUSTED_ORIGINS = ["https://fierce-thicket-99081.herokuapp.com"]
 
 
 import django_heroku
